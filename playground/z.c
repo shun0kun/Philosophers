5643e1191b103fbc92b81a0c4d99ae50c475f2cf
shunsuke <shun0classic@gmail.com>
2026-02-10 11:10:39 +0000

    home

--- playground/z.c
+++ playground/z.c
@@ -1,18 +1,48 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
 
-int	main(void)
+long long	current_unixtime_ms(void)
 {
-	int	fd;
+	struct timeval	tp;
 
-	fd = open("z", O_RDWR);
-	if (fd < 0)
+	gettimeofday(&tp, NULL);
+	return ((long long)tp.tv_sec * 1000 + (long long)tp.tv_usec / 1000);
+}
+
+//スレッド200でやったらどれくらい誤差が出るか調査する。
+void	wait_until_2(long long start, long long target)
+{
+	long long	now;
+	long long	remain;
+	
+	while (1)
 	{
-		perror("Open error");
-		return (1);
+		now = current_unixtime_ms();
+		remain = target - (now - start);
+		if (remain <= 0)
+			break ;
+		if (remain > 2)
+			usleep(1000);
+		else
+			usleep(500);
 	}
-	write(fd, "Helloo", 6);
-	close(fd);
-	return 0;
+}
+
+void	wait_until_1(long long start, long long target)
+{
+	while (current_unixtime_ms() - start <= target)
+		usleep(1000);//1msの誤差が生じる。
+}
+
+
+int	main(int argc, char **argv)
+{
+	long long	start;
+
+	start = current_unixtime_ms();
+	wait_until_1(start, atoi(argv[1]));
+	printf("%lld\n", current_unixtime_ms() - start);
 }
